44f6bbd8c534a8c9eda67272630483e838e5c6b3
Asger Hansen <asger.eh@gmail.com>
2026-04-18 14:37:56 +0200

    Move relay/button to class

---
+++ button.py
@@ -0,0 +1,50 @@
+import machine
+import time
+
+class Button:
+    def __init__(self, pin_id, debounce_ms=50):
+        # Using PULL_UP: 1 = Released, 0 = Pressed
+        self.pin = machine.Pin(pin_id, machine.Pin.IN, machine.Pin.PULL_UP)
+        self.debounce_ms = debounce_ms
+        self.last_time = time.ticks_ms()
+        
+        # Track the last stable state
+        self.state = self.pin.value()
+        # This flag "sticks" to True once a press is detected
+        self.was_pressed = False
+        
+        self.pin.irq(trigger=machine.Pin.IRQ_FALLING | machine.Pin.IRQ_RISING, handler=self._isr)
+        
+    def _isr(self, pin):
+        current_time = time.ticks_ms()
+        
+        # Debounce check
+        if time.ticks_diff(current_time, self.last_time) > self.debounce_ms:
+            new_state = pin.value()
+            
+            if new_state != self.state:
+                self.state = new_state
+                # If the new stable state is 0 (Pressed), set our sticky flag
+                if new_state == 0:
+                    self.was_pressed = True
+                    
+            self.last_time = current_time
+
+    def get_status(self):
+        """
+        Returns (was_pressed, is_pressed):
+        - was_pressed: True if a press occurred since the last call.
+        - is_pressed: True if the button is down RIGHT NOW.
+        """
+        irq_state = machine.disable_irq()
+        
+        # Grab the "sticky" flag and immediately reset it
+        was_pressed = self.was_pressed
+        self.was_pressed = False
+        
+        # Check current state (0 is pressed/down)
+        is_pressed = (self.state == 0)
+        
+        machine.enable_irq(irq_state)
+        
+        return was_pressed, is_pressed
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import machine
 import time
 import micropython
 
+from button import Button
 from config import pin
 from send_mail import send_mail
 
@@ -10,47 +11,16 @@ micropython.alloc_emergency_exception_buf(100)
 
 # Initialize LED and relay pins
 led = machine.Pin(pin.LED, machine.Pin.OUT)
-relay = machine.Pin(pin.RELAY, machine.Pin.IN, machine.Pin.PULL_UP)
+relay = Button(pin.RELAY)
 
-# Read initial state of the relay and set up a flag to track state changes
-# NOTE: these variables are accessed in both the main loop and the interrupt handler,
-# so they must be declared as global and protected by critical sections if necessary to avoid race conditions.
-relay_state = relay.value()
-relay_state_changed = False
-
-def relay_handler(relay):
-    global relay_state, relay_state_changed
-    relay_state = relay.value()
-    relay_state_changed = True
-
-relay.irq(handler=relay_handler,
-          trigger=machine.Pin.IRQ_FALLING | machine.Pin.IRQ_RISING)
-
-# Helper function to check if the relay state has changed, used in the main loop to avoid race conditions.
-def read_relay_state_changed():
-    global relay_state_changed, relay_state
-    irq_state = machine.disable_irq()
-    if relay_state_changed:
-        relay_state_changed = False
-        changed = True
-    else:
-        changed = False
-    new_relay_state = relay_state
-    machine.enable_irq(irq_state)
-    return changed, new_relay_state
-
-print('Init complete. Monitoring relay state...')
 while True:
-    changed, new_relay_state = read_relay_state_changed()
-    if changed:
-        if new_relay_state == 0:
-            print('Relay activated! Sending alert email...')
-            led.value(pin.LED_ON)
-            send_mail()
-        else:
-            print('Relay deactivated. System normal.')
+    was_pressed, is_pressed = relay.get_status()
+
+    if was_pressed:
+        led.value(pin.LED_ON)
+        send_mail()
     else:
-        if new_relay_state == 0:
+        if is_pressed:
             led.toggle()
         else:
             led.value(pin.LED_OFF)
